4f71a9f49dbaca40401311af73154ca3b81ee0bf
HaVinNNguyendev03 <nguyenvanphucdev03@gmail.com>
2024-07-05 10:57:06 +0700

    add comment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
+// import lib
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+// import layout
 import Header from './layouts/header/index.tsx'
 import Footer from './layouts/footer/index.tsx'
+// import from components
 import Login from './components/from/login/index.tsx'
 import './index.css'
-import './assets/css/main.css'
+import './assets/css/'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
