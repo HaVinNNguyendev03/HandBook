8252c95c284af96e2f21708a89e1bd2c91b7f3b1
HaVinNNguyendev03 <nguyenvanphucdev03@gmail.com>
2024-07-05 11:06:19 +0700

    main.css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import Footer from './layouts/footer/index.tsx'
 // import from components
 import Login from './components/from/login/index.tsx'
 import './index.css'
-import './assets/css/'
+import './assets/css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
